997ac8471a9305e01f3e2b056634678d71fe07a4
pas8 <65961364+pas8@users.noreply.github.com>
2020-11-24 22:12:42 +0200

    PAS_PAGE_V1

---
+++ BEST_PAGE/dist/index.dev.js
@@ -0,0 +1,13 @@
+// function draw() {
+//   var canvas = document.getElementById('canvas');
+//   if (canvas.getContext) {
+//     var ctx = canvas.getContext('2d');
+//     var rectangle = new Path2D();
+//     rectangle.rect(10, 10, 50, 50);
+//     var circle = new Path2D();
+//     circle.arc(100, 35, 25, 0, 2 * Math.PI);
+//     ctx.stroke(rectangle);
+//     ctx.fill(circle);
+//   }
+// }
+"use strict";
